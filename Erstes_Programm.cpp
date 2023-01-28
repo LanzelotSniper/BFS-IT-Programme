92bf3a10cc06d8c1980f6f39a82fab9c88c9bb96
LanzelotSniper <ardaokay28@gmail.com>
2023-01-28 19:26:14 +0100

    Update Erstes_Programm.cpp

--- Erstes_Programm.cpp
+++ Erstes_Programm.cpp
@@ -9,13 +9,13 @@ int main()
 	int b;
 	int c;
 	//Input-Output
-cout<<"Bitte geben sie den Preis des Produkts ein."<<endl;
+cout<<"Bitte geben Sie den Preis des Produkts ein."<<endl;
 cin>>a;
-cout<<"Bitte geben sie die Stueckzahl des Produkts ein."<<endl;
+cout<<"Bitte geben Sie die Stueckzahl des Produkts ein."<<endl;
 cin>>b;
 //Einfache Berechnung und Wertzuweisung auf Basis dieser
 c=a*b;
-cout<<"Der Gesammtpreis beraegt: "<<c<<endl;
+cout<<"Der Gesammtpreis betraegt: "<<c<<endl;
 //Standard Return
 return(0);
 }
