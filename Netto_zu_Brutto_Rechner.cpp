a8aca4f9fcb4a8dd885af3ed87abd0e0c3973a34
LanzelotSniper <ardaokay28@gmail.com>
2023-01-28 17:39:38 +0100

    Create Netto_zu_Brutto_Rechner.cpp

---
+++ Netto_zu_Brutto_Rechner.cpp
@@ -0,0 +1,27 @@
+//Netto zu Brutto Rechner
+#include <iostream>
+using namespace std;
+//Hauptfunktion
+int main()
+{
+	//Variablendeklaration
+	int preis;
+	int stueckzahl;
+	int nettopreis;
+	int mehrwertsteuer;
+	int bruttopreis;
+//IO-Sequenz und Berechnungen
+cout<<"Bitte geben sie den Preis des Produkts ein."<<endl;
+cin>>preis;
+cout<<"Bitte geben sie die Stueckzahl des Produkts ein."<<endl;
+cin>>stueckzahl;
+nettopreis=preis*stueckzahl;
+cout<<"Der Nettopreis betraegt: "<<nettopreis<<endl;
+mehrwertsteuer=0.19*c;
+cout<<"Die Mehrwertsteuer betraegt: "<<mehrwertsteuer<<endl;
+cout<<"----------------------------"<<endl;
+bruttopreis=mehrwertsteuer+c;
+cout<<"Der Bruttopreis beraegt: "<<bruttopreis<<endl;
+//Standard Return
+return(0);
+}
