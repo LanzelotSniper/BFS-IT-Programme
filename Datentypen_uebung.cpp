aa119f9016de0ccd659e5e225de6de508f1fdabd
LanzelotSniper <ardaokay28@gmail.com>
2023-01-29 02:06:35 +0100

    Create Datentypen_uebung.cpp

---
+++ Datentypen_uebung.cpp
@@ -0,0 +1,26 @@
+//Dieses Programm dient dazu den Umgang mit verschiedenen Datentypen zu Erklaeren
+#include <iostream>
+using namespace std;
+//Hauptfunktion
+int main()
+{
+//Variablen Deklaration
+int 	a;
+char 	x;
+string 	c;
+
+//Eingabe
+cout << "Bitte eine Zahl eingeben: ";
+cin >> a;
+cout << "Bitte einen Buchstaben eingeben: ";
+cin >> x;
+cout << "Bitte einen Namen eingeben: ";
+cin >> c;
+
+//Ausgabe
+cout << "Die Zahl war: " << a  << endl;
+cout << "Der Buhstabe war: " << x << endl;
+cout << "Der Name war: " << c << endl;
+
+}
+
